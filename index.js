d5ba9152c14a7dde162a5c8d6e67bf1510603962
felpino <felipeaugusto2002@sempreceub.com>
2024-11-04 10:23:33 -0300

    maqny changes

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get('/users', (req, res) => {
     res.send("Nessa rota retorno usuários");
 });
 
+app.post('/users', (req, res) => {
+    res.send("Nessa rota retorno usuários POST");
+});
+
+app.get('/user/:id', (req,res) => {
+    console.log(req.params);
+    res.send(`o parametro é ${req.params.id}`);
+});
+
 app.listen(PORT, () => {
     console.log(`SERVIDOR WEB OUVINDO NA PORTA ${PORT}`);
 });
